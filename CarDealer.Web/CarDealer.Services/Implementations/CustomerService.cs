284706cca5090f70823208a969b012883ed9c5a6
Dimitar Nikolov <zeitgeistdimi@gmail.com>
2017-11-10 20:04:11 +0200

    Refactoring some classes. Adding new library for the services and starting the query ordering.

---
+++ CarDealer.Web/CarDealer.Services/Implementations/CustomerService.cs
@@ -0,0 +1,48 @@
+﻿namespace CarDealer.Services.Implementations
+{
+    using Data;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Models;
+
+    class CustomerService : ICustomerService
+    {
+        private readonly CarDealerDbContext db;
+
+        public CustomerService(CarDealerDbContext db)
+        {
+            this.db = db;
+        }
+
+        public IEnumerable<CustomerModel> OrderedCustomers(OrderDirection order)
+        {
+            var customerQuery = this.db.Customers.AsQueryable();
+
+            switch (order)
+            {
+                case OrderDirection.Ascending:
+                    customerQuery = customerQuery
+                        .OrderBy(c => c.BirthDate)
+                        .ThenBy(c => c.Name);
+                    break;
+                case OrderDirection.Descending:
+                    customerQuery = customerQuery
+                        .OrderByDescending(c => c.BirthDate)
+                        .ThenBy(c => c.Name);
+                    break;
+                default:
+                    throw new InvalidOperationException($"Invalid order direction: {order}.");
+            }
+
+            return customerQuery
+                .Select(c => new CustomerModel
+                {
+                    Name = c.Name,
+                    BirthDate = c.BirthDate,
+                    IsYoungDriver = c.IsYoungDriver
+                })
+                .ToList();
+        }
+    }
+}
